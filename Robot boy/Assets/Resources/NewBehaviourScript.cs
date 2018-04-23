2b51596157347b65bc9503f714bb3b0f7248e5b7
ThaneAcheron <craig.turner944@gmail.com>
2018-04-23 11:28:43 +0200

    Final

---
+++ Robot boy/Assets/Resources/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void start() 
+	{
+
+	}
+	public void OnClick () 
+	{
+		Debug.Log ("Start");
+}
+}
